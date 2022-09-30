7668755f218ab19040fd978cfd28351f067a0a5e
Yasin Çoban <yasincoban1998@outlook.com>
2022-09-30 21:47:09 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles/main.scss';
+
+
+ReactDOM.render(<p>Starter Pack</p>,document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+module.exports = {
+  module:{
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader","sass-loader"]
+      }
+    ],
+},
+  entry: "./src/app.js",
+  mode:'production',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+},
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    }
+  },
+ 
+};
